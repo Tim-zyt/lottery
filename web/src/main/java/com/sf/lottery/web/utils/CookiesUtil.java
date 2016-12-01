b2a5e30b226123b57be807a569ef4fb80977c6a4
862911 <zhanggaoxin@sf-express.com>
2016-12-01 13:44:29 +0800

    添加Cookies操作类

---
+++ web/src/main/java/com/sf/lottery/web/utils/CookiesUtil.java
@@ -0,0 +1,114 @@
+package com.sf.lottery.web.utils;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Hash Zhang
+ * @version 1.0.0
+ * @date 2016/12/1.
+ */
+public class CookiesUtil {
+    /**
+     * 添加Cookies
+     * @param response
+     * @param name
+     * @param value
+     * @param maxAge 单位：秒
+     */
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge){
+        Cookie cookie = new Cookie(name.trim(), value.trim());
+        cookie.setMaxAge(maxAge);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 修改cookie
+     * @param request
+     * @param response
+     * @param name
+     * @param value
+     * @param maxAge 单位：秒
+     * 注意一、修改、删除Cookie时，新建的Cookie除value、maxAge之外的所有属性，例如name、path、domain等，都要与原Cookie完全一样。否则，浏览器将视为两个不同的Cookie不予覆盖，导致修改、删除失败。
+     */
+    public void editCookie(HttpServletRequest request,HttpServletResponse response,String name,String value, int maxAge){
+        Cookie[] cookies = request.getCookies();
+        if (null==cookies) {
+        } else {
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals(name)){
+                    cookie.setValue(value);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(maxAge);
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 删除cookie
+     * @param request
+     * @param response
+     * @param name
+     */
+    public void delCookie(HttpServletRequest request,HttpServletResponse response,String name){
+        Cookie[] cookies = request.getCookies();
+        if (null==cookies) {
+        } else {
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals(name)){
+                    cookie.setValue(null);
+                    cookie.setMaxAge(0);// 立即销毁cookie
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 根据名字获取cookie
+     * @param request
+     * @param name cookie名字
+     * @return
+     */
+    public static  Cookie getCookieByName(HttpServletRequest request,String name){
+        Map<String,Cookie> cookieMap = ReadCookieMap(request);
+        if(cookieMap.containsKey(name)){
+            Cookie cookie = (Cookie)cookieMap.get(name);
+            return cookie;
+        }else{
+            return null;
+        }
+    }
+
+
+
+    /**
+     * 将cookie封装到Map里面
+     * @param request
+     * @return
+     */
+    private static  Map<String,Cookie> ReadCookieMap(HttpServletRequest request){
+        Map<String,Cookie> cookieMap = new HashMap<String,Cookie>();
+        Cookie[] cookies = request.getCookies();
+        if(null!=cookies){
+            for(Cookie cookie : cookies){
+                cookieMap.put(cookie.getName(), cookie);
+            }
+        }
+        return cookieMap;
+    }
+}
