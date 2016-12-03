b81a8c5b8a305023217969a3d5c4d8412a7a4b6d
01139954 <515043638@qq.com>
2016-12-03 10:02:34 +0800

    修改字体

--- web/src/webapps/frontend/js/signwall.js
+++ web/src/webapps/frontend/js/signwall.js
@@ -12,6 +12,12 @@ jQuery(function ($) {
             },
             success: function(data){
                 var signedusers = data.data;
+                var userHtml = "";
+                var iLen = signedusers.length;
+                for(var i = 0 ; i < iLen ; i++){
+                    userHtml += "<li style='width: 10%;'><img src='"+ signedusers[i].wxHeadimgurl +"'alt='User Image'><a href='#' style='font-size: 20px;font-family: 微软雅黑, Microsoft YaHei;color: #0099FF;' class='users-list-name'>" + signedusers[i].sfName + "</a></li>";
+                }
+                $("#users").html(userHtml);
             }
         });
     }
