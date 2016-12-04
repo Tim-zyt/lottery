dafba11227d03f37fada6c86bf60b75ad2d7add2
01139954 <515043638@qq.com>
2016-12-04 17:39:07 +0800

    隐藏socket代码

--- web/src/webapps/frontend/js/signwall.js
+++ web/src/webapps/frontend/js/signwall.js
@@ -2,21 +2,21 @@ jQuery(function ($) {
     $(document).ready(function () {
         time();
 
-        var ws = new WebSocket(signWallChannelAddress);
-        ws.onopen = function(){
-        };
-        ws.onmessage = function(message){
-            // var user = message.data;
-            // var signUserHtml = "<li style='width: 10%;'><img src='"+ user.wxHeadimgurl +"'alt='User Image'><a href='#' style='font-size: 20px;font-family: 微软雅黑, Microsoft YaHei;color: #0099FF;' class='users-list-name'>" + user.sfName + "</a></li>";
-            // $("#users").prepend(signUserHtml);
-        };
-        function postToServer(){
-            ws.send(document.getElementById("msg").value);
-            document.getElementById("msg").value = "";
-        }
-        function closeConnect(){
-            ws.close();
-        }
+        // var ws = new WebSocket(signWallChannelAddress);
+        // ws.onopen = function(){
+        // };
+        // ws.onmessage = function(message){
+        //     // var user = message.data;
+        //     // var signUserHtml = "<li style='width: 10%;'><img src='"+ user.wxHeadimgurl +"'alt='User Image'><a href='#' style='font-size: 20px;font-family: 微软雅黑, Microsoft YaHei;color: #0099FF;' class='users-list-name'>" + user.sfName + "</a></li>";
+        //     // $("#users").prepend(signUserHtml);
+        // };
+        // function postToServer(){
+        //     ws.send(document.getElementById("msg").value);
+        //     document.getElementById("msg").value = "";
+        // }
+        // function closeConnect(){
+        //     ws.close();
+        // }
         getSignedUser();
     });
 
