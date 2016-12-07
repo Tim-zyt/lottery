bebc91dfd1452ee0ff9b7eb36cc6c6f2ce372801
01139940 <zhangyutao3@sf-express.com>
2016-12-07 20:08:38 +0800

    cp签到前端

--- web/src/main/java/com/sf/lottery/web/user/controller/DanmukuController.java
+++ web/src/main/java/com/sf/lottery/web/user/controller/DanmukuController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
