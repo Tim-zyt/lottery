b39739fbc19bcdcef6e7a177293f93f4f451aab6
01139940 <zhangyutao3@sf-express.com>
2016-12-07 20:08:38 +0800

    cp签到前端

--- web/src/webapps/frontend/js/cpsign.js
+++ web/src/webapps/frontend/js/cpsign.js
@@ -18,11 +18,8 @@ $.ajax({
 
 wx.ready(function(){
 });
-//
-var last_update = new Date().getTime();
-var last_x =0;
-var last_y=0;
-var last_z= 0;
+
+
 jQuery(function ($) {
     $(document).ready(function () {
         $(".cpSignup").click(function () {
@@ -32,47 +29,23 @@ jQuery(function ($) {
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
                     var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    alert(localIds);
                     wx.uploadImage({
                         localId: localIds[0], // 需要上传的图片的本地ID，由chooseImage接口获得
                         isShowProgressTips: 1, // 默认为1，显示进度提示
                         success: function (res2) {
                             var serverId = res2.serverId; // 返回图片的服务器端ID
-                            $("p.login-box-msg").html(serverId);
+                            showImg(serverId);
+                            // $("p.login-box-msg").html(serverId);
                         }
                     });
                 }
             });
         });
-        if (window.DeviceMotionEvent) {
-            window.addEventListener('devicemotion', deviceMotionHandler, false);
-        } else {
-            alert('你的手机太差了，不能参与摇一摇抽奖，扔掉买个新的吧。--顺丰科技忠劝');
-        }
     });
-    var shakeTimes = 0;
-    function deviceMotionHandler(eventData) {
-        var acceleration = eventData.accelerationIncludingGravity;
-        var curTime = new Date().getTime();
-
-        if ((curTime - last_update) > 100) {
-            var diffTime = curTime - last_update;
-            last_update = curTime;
-            var x = acceleration.x;
-            var y = acceleration.y;
-            var z = acceleration.z;
-            var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-            var status = document.getElementById("status");
-
-            if (speed > 1000) {
-                $("p.login-box-msg").html(shakeTimes++);
-            }
-            last_x = x;
-            last_y = y;
-            last_z = z;
-        }
+    
+    function showImg(serverId) {
+        var imgSrc = "";
+        $("#cpDiv").css("display","block");
+        $("#cpImg").attr("src",imgSrc);
     }
 });
-
-
-
