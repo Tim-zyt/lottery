466de0584c895b3edee5de1b579d30307e2a9ad3
AntonyWujiang <e0012443@u.nus.edu>
2016-12-20 16:53:30 +0800

    摇一摇UI改版；增加打印CP照片功能；初始化bug修复

---
+++ web/src/webapps/frontend/js/manage/manage.js
@@ -0,0 +1,49 @@
+/**
+ * Created by 01170626 on 2016/12/7.
+ */
+function resetUsers() {
+    var flag = confirm("确认初始化吗？");
+    if(flag){
+        $.ajax({
+            type: "post",
+            url : getContextPath() + "/reset/resetData",
+            dataType:'json',
+            data: {
+            },
+            success: function(data){
+                var flag = data.data;
+                if(flag){
+                    // layer.msg('删除成功', {
+                    //     time: 500, //20s后自动关闭
+                    //     // btn: ['明白了', '知道了']
+                    // });
+                    alert("数据库初始化成功");
+                }else{
+                    alert("数据库初始化失败失败");
+                }
+            }
+        });
+    }
+}
+
+function printCouple(){
+    $.ajax({
+        type: "get",
+        url : getContextPath() + "/couple/printCouples",
+        dataType:'json',
+        data: {
+        },
+        success: function(data){
+            var flag = data.data;
+            if(flag){
+                // layer.msg('删除成功', {
+                //     time: 500, //20s后自动关闭
+                //     // btn: ['明白了', '知道了']
+                // });
+                alert("打印成功");
+            }else{
+                alert("数据库初始化失败失败");
+            }
+        }
+    });
+}
