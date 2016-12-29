b239e78639299a6cea563e1eaac3af8d29fcbde8
01139940 <zhangyutao3@sf-express.com>
2016-12-29 13:46:37 +0800

    顺手付红包+入门有礼

--- web/src/webapps/frontend/js/sfPay.js
+++ web/src/webapps/frontend/js/sfPay.js
@@ -1,80 +1,89 @@
 jQuery(function ($) {
     $(document).ready(function () {
         var payImg = $("div#sfpay");
-        var checkIfStart = setInterval(function checkStart() {
+
+        function checkStart() {
             $.ajax({
                 type: "post",
                 url: getContextPath() + "/pay/isCanSfPay",
                 dataType: 'json',
                 data: {},
-                success: function judge(data) {
+                success: function (data) {
                     var temp = data.data;
-                    if (temp == 1) {
-                        payImg.html("");
-                        payImg.append($("<a href='http://www.baidu.com'>点我抢顺手付红包啦1</a>"));
-                        payImg.removeClass("animated tada");
-                        payImg.addClass("animated tada");
-                        clearInterval(checkIfStart);
 
-                        var payInterval = setInterval(function () {
+                    function judge() {
+                        if (temp == 1) {
+                            payImg.html("");
+                            payImg.append($("<a href='http://www.baidu.com' style='font-size:25px;font-family: Microsoft Yahei;color: white'>点我抢顺手付红包啦1</a>"));
+                            payImg.removeClass("animated tada");
+                            payImg.addClass("animated tada");
+
                             $.ajax({
                                 type: "post",
                                 url: getContextPath() + "/pay/isCanSfPay",
                                 dataType: 'json',
                                 data: {},
                                 success: function (data) {
-                                    judge(data);
+                                    temp = data.data;
+                                    setTimeout(judge, 1000);
+                                }, error: function () {
+                                    setTimeout(judge, 1000);
                                 }
                             });
-                        }, 1000);
-                    } else if(temp == 2){
-                        payImg.html("");
-                        payImg.append($("<a href='http://www.sogou.com'>点我抢顺手付红包啦2</a>"));
-                        payImg.removeClass("animated tada");
-                        payImg.addClass("animated tada");
-                        clearInterval(checkIfStart);
 
-                        var payInterval = setInterval(function () {
+                        } else if (temp == 2) {
+                            payImg.html("");
+                            payImg.append($("<a href='http://www.sogou.com' style='font-size:25px;font-family: Microsoft Yahei;color: white'>点我抢顺手付红包啦2</a>"));
+                            payImg.removeClass("animated tada");
+                            payImg.addClass("animated tada");
+
                             $.ajax({
                                 type: "post",
                                 url: getContextPath() + "/pay/isCanSfPay",
                                 dataType: 'json',
                                 data: {},
                                 success: function (data) {
-                                    judge(data);
+                                    temp = data.data;
+                                    setTimeout(judge, 1000);
+                                }, error: function () {
+                                    setTimeout(judge, 1000);
                                 }
                             });
-                        }, 1000);
-                    } else if(temp == 3){
-                        payImg.html("");
-                        payImg.append($("<a href='http://www.so.com'>点我抢顺手付红包啦3</a>"));
-                        payImg.removeClass("animated tada");
-                        payImg.addClass("animated tada");
-                        clearInterval(checkIfStart);
+                        } else if (temp == 3) {
+                            payImg.html("");
+                            payImg.append($("<a href='http://www.so.com' style='font-size:25px;font-family: Microsoft Yahei;color: white''>点我抢顺手付红包啦3</a>"));
+                            payImg.removeClass("animated tada");
+                            payImg.addClass("animated tada");
 
-                        var payInterval = setInterval(function () {
                             $.ajax({
                                 type: "post",
                                 url: getContextPath() + "/pay/isCanSfPay",
                                 dataType: 'json',
                                 data: {},
                                 success: function (data) {
-                                    var temp = data.data;
-                                    if (temp == 0) {
-                                        clearInterval(payInterval);
-                                        checkIfStart = setInterval(checkStart, 2000);
-                                        payImg.html("");
-                                        payImg.append($("<h3 class='animated bounceIn'>顺手付红包还未开始或已结束，请等待主持人安排</h3>"));
-                                    }
+                                    temp = data.data;
+                                    setTimeout(judge, 1000);
+                                }, error: function () {
+                                    setTimeout(judge, 1000);
                                 }
                             });
-                        }, 1000);
-                    } else {
-                        payImg.html("");
-                        payImg.append($("<h3 class='animated bounceIn'>顺手付红包还未开始或已结束，请等待主持人安排</h3>"));
+                        } else {
+                            payImg.html("");
+                            payImg.append($("<h3 class='animated bounceIn'>顺手付红包还未开始或已结束，请等待主持人安排</h3>"));
+                            setTimeout(checkStart, 2000);
+                        }
                     }
+
+                    judge();
+                },
+                error: function () {
+                    setTimeout(checkStart, 2000);
                 }
             });
-        }, 2000);
+
+        }
+
+        checkStart();
     });
+
 });
